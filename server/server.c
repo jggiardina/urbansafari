146f5c0ccf96e33432f8b0084bf08b4bfa86077b
Jeffrey Giardina <jggiardina@gmail.com>
2013-02-08 15:52:28 -0500

    renamed all makefile to Makefile. added Makefile and config.mk in the root directory as given in TheDoctor. overwrote client/client.c and server/server.c with skeleton code given in the assignment. added lib/protocol_(client/server/session/utils).[c/h] as given in the assignment

--- server/server.c
+++ server/server.c
@@ -22,118 +22,102 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <pthread.h>
+#include <sys/types.h>
+#include <poll.h>
+#include "../lib/types.h"
+#include "../lib/protocol_server.h"
+#include "../lib/protocol_utils.h"
 
-#include "../lib/net.h"
-#include "../lib/net.c"
-
-#define MAXLINE 80
-
-struct Globals {
-  int verbose;
-} globals;
-
-#define VPRINTF(fmt, ...)					  \
-  {								  \
-    if (globals.verbose==1) fprintf(stderr, "%s: " fmt,           \
-				    __func__, ##__VA_ARGS__);	  \
-  }
-
-void
-str_echo(int sockfd)
+int 
+doUpdateClients(void)
 {
-  int n;
-  int  len;
-  char *buf;
-  
-  while (1) {
-    n = net_readn(sockfd, &len, sizeof(int));
-    if (n != sizeof(int) && n != 0) { // The length cannot be for a valid message, if it is 0, we know a client disconnected abruptly.
-      fprintf(stderr, "%s: ERROR failed to read len: %d!=%d"
-	      " ... closing connection\n", __func__, n, (int)sizeof(int));
-      break;
-    }
-    /*Display that client closed the connection and return the child*/
-    else if (n == 0) {
-      fprintf(stderr, "Client closed on socket: %d. GOODBYE!\n", sockfd);
-      break;
-    }
- 
-    //WHAT DOES THE NEXT LINE DO?
-      len = ntohl(len);//from network byte order to host byte order -- swaps the bite order 
-    if (len) {
-      buf = (char *)malloc(len);
-      n = net_readn(sockfd, buf, len);
-      if ( n != len ) {
-	fprintf(stderr, "%s: ERROR failed to read msg: %d!=%d"
-		" .. closing connection\n" , __func__, n, len);
-	break;
-      }
-      VPRINTF("got: %d '%s'\n", len, buf);
-      net_writen(sockfd, buf, len);
-      free(buf);
-    }
+  Proto_Session *s;
+  Proto_Msg_Hdr hdr;
+
+  s = proto_server_event_session();
+  hdr.type = PROTO_MT_EVENT_BASE_UPDATE;
+  proto_session_hdr_marshall(s, &hdr);
+  proto_server_post_event();  
+  return 1;
+}
+
+char MenuString[] =
+  "d/D-debug on/off u-update clients q-quit";
+
+int 
+docmd(char cmd)
+{
+  int rc = 1;
+
+  switch (cmd) {
+  case 'd':
+    proto_debug_on();
+    break;
+  case 'D':
+    proto_debug_off();
+    break;
+  case 'u':
+    rc = doUpdateClients();
+    break;
+  case 'q':
+    rc=-1;
+    break;
+  case '\n':
+  case ' ':
+    rc=1;
+    break;
+  default:
+    printf("Unkown Command\n");
   }
-  close(sockfd);
-  return;
+  return rc;
+}
+
+int
+prompt(int menu) 
+{
+  int ret;
+  int c=0;
+
+  if (menu) printf("%s:", MenuString);
+  fflush(stdout);
+  c=getchar();;
+  return c;
 }
 
 void *
-doit(void *arg)
+shell(void *arg)
 {
-  long val = (long)arg;
-  pthread_detach(pthread_self());
-  str_echo(val);
-  close(val);
+  int c;
+  int rc=1;
+  int menu=1;
+
+  while (1) {
+    if ((c=prompt(menu))!=0) rc=docmd(c);
+    if (rc<0) break;
+    if (rc==1) menu=1; else menu=0;
+  }
+  fprintf(stderr, "terminating\n");
+  fflush(stdout);
   return NULL;
 }
 
 int
 main(int argc, char **argv)
-{
-  int listenfd, port=0;
-  long connfd;
-  pthread_t tid;
-  
-  // set port number if it is passed as a command line arg
-  int newport = 0;
-  if (argc == 2) {
-    // check here for valid/acceptable port number
-    newport = atoi(argv[1]);
-    if (newport > 5000 && newport < 49152)
-      port = newport;
-    else {
-      fprintf(stderr, "port number %d INVALID! Must be greater than 5000 and less than 49152\n", newport); 
-      exit(-1);
-    }
-  }
-
-  bzero(&globals, sizeof(globals));
-
-  if (net_setup_listen_socket(&listenfd, &port) < 0) {
-    fprintf(stderr, "net_setup_listen_socket FAILED!\n");
+{ 
+  if (proto_server_init()<0) {
+    fprintf(stderr, "ERROR: failed to initialize proto_server subsystem\n");
     exit(-1);
   }
 
-  printf("listening on port=%d\n", port);
+  fprintf(stderr, "RPC Port: %d, Event Port: %d\n", proto_server_rpcport(), 
+	  proto_server_eventport());
 
-  if (net_listen(listenfd) < 0) {
-    fprintf(stderr, "Error: server listen failed (%d)\n", errno);
+  if (proto_server_start_rpc_loop()<0) {
+    fprintf(stderr, "ERROR: failed to start rpc loop\n");
     exit(-1);
   }
+    
+  shell(NULL);
 
-  for (;;) {
-    connfd = net_accept(listenfd);
-    if (connfd < 0) {
-      fprintf(stderr, "Error: server accept failed (%d)\n", errno);
-    } else {
-      //EXPLAIN WHAT IS HAPPENING HERE IN YOUR LOG
-      pthread_create(&tid, NULL, &doit, (void *)connfd);
-    }
-  }
-
-  VPRINTF("Exiting\n");
+  return 0;
 }
