0b034c825fc6e0efdfe157b59728a5733166c6ec
Ryan Conrad <rconrad54@gmail.com>
2013-03-19 14:24:44 -0400

    Adding in the maze.c and maze.h files, will not make though just initial setup

---
+++ lib/maze.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <strings.h>
+#include <errno.h>
+#include <pthread.h>
+#include <assert.h>
+
+#include "misc.h"
+#include "maze.h"
+
+int 
---
+++ lib/maze.h
@@ -0,0 +1,42 @@
+#include "net.h"
+#include "protocol.h"
+#include "protocol_session.h"
+
+typedef enum {JAIL, HOME, FLOOR} Cell_Type;
+
+typedef enum {RED, GREEN} Color;
+
+typedef struct 
+{
+  int x;
+  int y;
+} __attribute__((__packed__)) Pos;
+
+typedef struct
+{
+  Pos p;
+  Color c;
+} __attribute__((__packed__)) Flag;
+
+typedef struct
+{
+  Pos p;
+  int charges=10;
+} __attribute__((__packed__)) Hammer;
+
+typedef struct
+{
+  Pos p;
+  Hammer h; //Dont know if we want these
+  Flag f;  //Dont know if we want these 
+  Cell_Type t;
+} __attribute__((__packed__)) Cell;
+
+typedef struct
+{
+  int h;
+  int w;
+  Cell[] cells;
+  int num_walls_cells;
+  int num_floor_cells;
+} __attribute__((__packed__)) Map;
